0017f2cf2009ddf64181be022612a4a34b8c3842
Taeho Kim <taeho@taeho.io>
2021-05-22 12:42:25 +0900

    Update react-notion-x (#11)

--- const.ts
+++ const.ts
@@ -1,5 +1,1 @@
 export const gaTraceId = 'G-HDWXJVK15N';
-
-export const isDevelopmentEnv =
-  process.env.NODE_ENV === 'development' || !process.env.NODE_ENV;
-export const isProductionEnv = process.env.NODE_ENV === 'production';
--- server-preload.js
+++ server-preload.js
@@ -1,8 +1,6 @@
 // @ts-check
 'use strict';
 
-const isProductionEnv = process.env.NODE_ENV === 'production';
-
 function setUpGoogleCloudProfiler() {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('@google-cloud/profiler').start({
@@ -12,6 +10,6 @@ function setUpGoogleCloudProfiler() {
   });
 }
 
-if (isProductionEnv) {
+if (process.env.SHOULD_PROFILE) {
   setUpGoogleCloudProfiler();
 }
