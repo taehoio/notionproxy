a98f9d508f243bb1b15c7f125b008435707b0886
Taeho Kim <taeho@taeho.io>
2021-05-23 10:37:37 +0900

    Add google cloud tracing

--- server-preload.js
+++ server-preload.js
@@ -1,15 +1,36 @@
 // @ts-check
 'use strict';
 
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const serviceName = 'notionProxy';
+
 function setUpGoogleCloudProfiler() {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('@google-cloud/profiler').start({
     serviceContext: {
-      service: 'notionproxy',
+      service: serviceName,
     },
+  });
+}
+
+function setUpGoogleCloudTracing() {
+  const { OCAgentExporter } = require('@opencensus/exporter-ocagent');
+  const exporter = new OCAgentExporter({
+    serviceName: serviceName,
+  });
+
+  const tracing = require('@opencensus/nodejs');
+  tracing.start({
+    exporter: exporter,
+    samplingRate: 1.0,
+    logLevel: 1,
   });
 }
 
 if (process.env.SHOULD_PROFILE) {
   setUpGoogleCloudProfiler();
+}
+
+if (process.env.SHOULD_TRACE) {
+  setUpGoogleCloudTracing();
 }
