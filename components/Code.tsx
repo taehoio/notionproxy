fd4e4da01e0e2f3c03395ce3062abc220731cb4d
Taeho Kim <taeho@taeho.io>
2021-06-01 16:12:59 +0900

    Add prismjs makefile support

--- components/Code.tsx
+++ components/Code.tsx
@@ -10,6 +10,7 @@ import 'prismjs/components/prism-java';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-json';
 import 'prismjs/components/prism-jsx';
+import 'prismjs/components/prism-makefile';
 import 'prismjs/components/prism-markdown';
 import 'prismjs/components/prism-markup';
 import 'prismjs/components/prism-protobuf';
