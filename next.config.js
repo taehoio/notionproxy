3e6a9c2cee649e4d32f9250276ff989c635871f0
Taeho Kim <taeho@taeho.io>
2021-06-20 07:50:56 +0900

    remove

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 // @ts-check
 'use strict';
 
-/* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require('fs');
-
-const pageIdsThatHaveThumnail = [];
-
-fs.readdirSync('./public/images/thumbnails/pages/').forEach((filename) => {
-  pageIdsThatHaveThumnail.push(filename.split('.')[0]);
-});
-
 const nextConfig = {
   async rewrites() {
     return [
@@ -24,13 +15,13 @@ const nextConfig = {
     rootNotionPageId: '6ca2dd5e-2214-4873-8ffa-d634d8ebbb53',
     rootNotionSpaceId: 'f2b37586-33c0-4236-bab4-a87c12f0d6e9',
     gaTraceId: 'G-HDWXJVK15N',
-    pageIdsThatHaveThumnail,
   },
 };
 
 module.exports = nextConfig;
 
 if (process.env.ANALYZE === 'true') {
+  /* eslint-disable @typescript-eslint/no-var-requires */
   const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
   });
