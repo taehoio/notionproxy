b416193d0737ee69ab7b8d3686e74426dde18f8c
Taeho Kim <taeho@taeho.io>
2022-07-03 09:26:37 +0900

    Add graceful shutdown

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
+    'plugin:@next/next/recommended',
   ],
   root: true,
   env: {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -36,3 +36,15 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+// Manual graceful shutdowns
+if (process.env.NEXT_MANUAL_SIG_HANDLE) {
+  process.on('SIGTERM', () => {
+    console.log('Received SIGTERM: ', 'cleaning up');
+    process.exit(0);
+  });
+  process.on('SIGINT', () => {
+    console.log('Received SIGINT: ', 'cleaning up');
+    process.exit(0);
+  });
+}
