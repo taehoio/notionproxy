9281ec683595305ae152ac87e75ee99e9669b286
Taeho Kim <taeho@taeho.io>
2022-07-09 19:05:50 +0900

    Revert "Bump react-notion-x" (#64)
    Revert "Bump react-notion-x (#63)"
    
    This reverts commit cd8985b424db56e9bc5cc71632a996119c13033d.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,9 @@ import 'react-notion-x/src/styles.css';
 // used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-tomorrow.css';
 
+// used for collection views (optional)
+import 'rc-dropdown/assets/index.css';
+
 // used for rendering equations (optional)
 import 'katex/dist/katex.min.css';
 
